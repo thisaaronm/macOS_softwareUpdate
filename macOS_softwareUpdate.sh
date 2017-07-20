24f5c85b55e93b90be777f030d91204466c5542e
Aaron M <thisaaronm@users.noreply.github.com>
2017-07-20 16:15:38 -0700

    Add macOS_softwareUpdate.sh

---
+++ macOS_softwareUpdate.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+
+################################################################################
+# Checks to see if user is root.
+# If not, it exits the script.
+################################################################################
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Must be run as root."
+	echo "Exiting..."
+	sleep 3
+	exit 1
+fi
+
+
+################################################################################
+# Checks to see if there are any software updates.
+# If not, it exits the script.
+################################################################################
+function f_check_for_software_updates() {
+	echo
+	echo "Checking for software updates. Please wait..."
+	echo
+
+	v_sw_status=$(softwareupdate -l | grep -i "recommended")
+
+	if [ -z "$v_sw_status" ]; then
+		echo "There are no updates available."
+		echo "Exiting..."
+		sleep 3
+		echo
+		exit
+	else
+		echo "The following updates are available: "
+		echo "$v_sw_status"
+		sleep 3
+	fi
+}
+
+
+################################################################################
+# Checks to see if this version of macOS supports FDE AuthRestart.
+################################################################################
+function f_check_authrestart_status() {
+	v_authrestart_check=$(fdesetup supportsauthrestart)
+
+	if [ "$v_authrestart_check" == true ]; then
+		echo "true"
+	elif [ "$v_authrestart_check" == false ]; then
+		echo "false"
+	fi
+}
+
+
+################################################################################
+# Checks to see if FileVault is ON or OFF.
+################################################################################
+function f_check_filevault_status() {
+	v_fde_status=$(fdesetup status)
+
+	if [ "$v_fde_status" == "FileVault is On." ]; then
+		echo "true"
+	elif [ "$v_fde_status" == "FileVault is Off." ]; then
+		echo "false"
+	fi
+}
+
+
+################################################################################
+# Checks if the user wishes to proceed with the sw update with no AuthRestart.
+################################################################################
+function f_sw_update_no_authrestart() {
+	choices=("Yes" "No")
+	echo "Proceed with software update?"
+	select choice in "${choices[@]}";
+	do
+		case $choice in
+			Yes)
+				echo "Software update will begin..."
+				echo
+				bash -c "softwareupdate -ia && reboot"
+				;;
+			No)
+				echo "Software update will not begin..."
+				sleep 1
+				echo "Exiting..."
+				echo
+				sleep 3
+				exit
+				;;
+		esac
+	done
+}
+
+
+################################################################################
+# Checks how user wants to unlock FileVault, then updates accordingly.
+################################################################################
+function f_check_filevault_unlock_and_update() {
+	choices=("automatically" "manually")
+	echo "After installing updates, would you like to unlock FileVault: "
+	select choice in "${choices[@]}"
+	do
+		case $choice in
+			"automatically")
+				echo "You will be prompted for your passphrase prior to reboot. This will,"
+				echo "upon completion of the software update installation, automatically"
+				echo "unlock FileVault, bringing you straight to the login screen."
+				echo
+				echo "If you want to cancel, press CTRL+C to EXIT."
+				echo "Otherwise, the software update will begin in 10 seconds..."
+				echo
+				sleep 10
+				bash -c "softwareupdate --install --all && fdesetup authrestart"
+				;;
+			"manually")
+				echo "You will not be prompted for your passphrase prior to reboot."
+				echo "This means that you will not skip the unlock screen, and will,"
+				echo "upon completion of the software update installation, be required"
+				echo "to manually unlock FileVault, prior to arriving at the login screen."
+				echo
+				echo "If you want to cancel, press CTRL+C to EXIT."
+				echo "Otherwise, the software update will begin in 10 seconds..."
+				echo
+				sleep 10
+				bash -c "softwareupdate --install --all && reboot"
+				;;
+		esac
+	done
+}
+
+
+################################################################################
+# RUN IT!
+################################################################################
+f_check_for_software_updates
+v_authrestart=$(f_check_authrestart_status) # true or false
+v_filevault=$(f_check_filevault_status) # true or false
+
+if [ "$v_authrestart" == true ]; then
+	if [ "$v_filevault" == true ]; then
+		f_check_filevault_unlock_and_update
+	elif [ "$v_filevault" == false ]; then
+		f_sw_update_no_authrestart
+	fi
+elif [ "$v_authrestart" == false ]; then
+	f_sw_update_no_authrestart
+fi
